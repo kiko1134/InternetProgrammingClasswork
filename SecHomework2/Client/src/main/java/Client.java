b1d8234cd84abe9defcb06c70ae4ca7940295ac0
unknown <krisvas8aelsys@gmail.com>
2021-10-31 09:50:07 +0200

    Second homework part 2

---
+++ SecHomework2/Client/src/main/java/Client.java
@@ -0,0 +1,113 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
+import java.util.Scanner;
+
+import static java.lang.System.console;
+import static java.lang.System.exit;
+
+public class Client {
+
+    public static int clientStatus(String host, int port) {
+        try {
+            Socket socket = new Socket(host, port);
+            InetAddress domainInetAddress = null;
+            domainInetAddress = InetAddress.getByName(host);
+            try {
+                socket.close();
+            } catch (IOException i) {
+            }
+
+        } catch (UnknownHostException u) {
+            return 1;
+        } catch (ConnectException e) {
+            return -1;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return 0;
+    }
+
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+
+    public void startConnection(String ip, int port) throws IOException {
+        clientSocket = new Socket(ip, port);
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public String sendMessage(String msg) throws IOException {
+        out.println(msg);
+        String resp = in.readLine();
+        return resp;
+    }
+
+    public void stopConnection() throws IOException {
+        in.close();
+        out.close();
+        clientSocket.close();
+    }
+
+    public PrintWriter getOut() {
+        return out;
+    }
+
+    public BufferedReader getIn() {
+        return in;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) {
+            System.err.println("invalid arguments");
+            exit(1);
+        }
+        Scanner reader = new Scanner(System.in);
+        String[] input = args[0].split(":");
+        String host = input[0];
+        int port = Integer.parseInt(input[1]);
+
+        if (port < 0 || port >= 65536) {
+            System.err.println("invalid arguments");
+            exit(1);
+        }
+
+        int response = clientStatus(host, port);
+
+        if (response == 1) {
+            System.err.println("invalid host");
+            exit(3);
+        }
+
+        if (response == -1) {
+            System.err.println("connection not possible");
+            exit(4);
+        }
+
+        Client client = new Client();
+        client.startConnection(host, port);
+        ExitHandler handler = new ExitHandler(host,port);
+        handler.start();
+
+        while (true) {
+            System.out.print("Client: ");
+            if (reader.hasNextLine()) {
+                String in = reader.nextLine();
+                String operation = client.sendMessage(in);
+                if (operation == null) {
+                    System.out.println("server disconnect");
+                    System.exit(0);
+                }
+                System.out.println(operation);
+            }
+
+        }
+
+//        client.stopConnection();
+    }
+}
---
+++ SecHomework2/Server/src/main/java/Server.java
@@ -0,0 +1,147 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static java.lang.System.*;
+
+public class Server {
+    private ServerSocket serverSocket;
+
+    public static boolean portStatus(int port) {
+        boolean free;
+        try (var ignored = new ServerSocket(port)) {
+            free = true;
+        } catch (IOException e) {
+            free = false;
+        }
+        return free;
+    }
+
+    private void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            new ServerConnection(clientSocket).start();
+        }
+    }
+
+    static class ServerConnection extends Thread {
+        private final Socket clientSocket;
+
+
+        ServerConnection(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        public void run() {
+            try {
+                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                String operation;
+
+                while (true) {
+                    operation = in.readLine();
+                    int status = checkOperation(operation);
+
+                    if (operation.equalsIgnoreCase("exit") || operation.equalsIgnoreCase("quit")) {
+                        break;
+                    }
+
+                    if (status == 0) {
+                            String[] input = operation.split(" ");
+                            String zone_offset = input[1];
+
+                            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
+                            ZoneOffset zoneOffset = ZoneOffset.of(zone_offset);
+                            OffsetTime time = OffsetTime.now(zoneOffset);
+                            LocalTime l_time = time.toLocalTime();
+                            out.println("Server: " + dtf.format(l_time));
+                    }
+
+                    if (status == 1) {
+                        out.println("Server: invalid arguments");
+                    }
+                    if (status == 2) {
+                        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
+                        LocalDateTime now = LocalDateTime.now();
+                        out.println("Server: " + dtf.format(now));
+                    }
+
+                    if (status == 3)
+                        out.println("Server: invalid time zone");
+
+                }
+
+                in.close();
+                out.close();
+                clientSocket.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private static int checkOperation(String operation) {
+        try {
+            if (operation.equals("time"))
+                return 2;
+
+            String[] input = operation.split(" ");
+
+            if (!input[0].equals("time") || input.length > 2) {
+                return 1;
+            }
+
+            if (input[1].charAt(0) != '+' && input[1].charAt(0) != '-') {
+                return 3;
+            }
+            if (input[0].equals("time") && (input[1].charAt(0) == '+' || input[1].charAt(0) != '-')) {
+                try{
+                    String zone_offset = input[1];
+                    ZoneOffset zoneOffset = ZoneOffset.of(zone_offset);
+                }catch (Exception e){return 1;}
+
+
+            }
+
+        } catch (Exception i) {
+            i.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) {
+            System.err.println("invalid arguments");
+            exit(1);
+        }
+        int port = Integer.parseInt(args[0]);
+
+        if (port < 0 || port >= 65536) {
+            System.err.println("invalid arguments");
+            exit(1);
+        }
+//        System.out.println(args[0]);
+        if (!portStatus(port)) {
+            System.err.println("port is already in use");
+            exit(2);
+        }
+
+        Server server = new Server();
+        server.start(port);
+
+
+
+    }
+
+
+}
